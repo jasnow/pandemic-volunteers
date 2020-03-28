3b289f57d5592ffc0bce72394368bcf65ac3bb73
Michael Kopchick <kopchickm@gmail.com>
2020-03-28 11:01:50 -0400

    Start on partners view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post '/', to: 'volunteers#create' # POST /#apply for the volunteer form on the root
 
   resources :cities, only: :index
+  resources :partners, only: [:index, :create]
   resources :states, only: :index
 end
